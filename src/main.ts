b05881e64890ccf3feb1914670723a90eb7936e5
Murat Jummyyev <muratjummyyev@Murats-MacBook-Air.local>
2022-08-09 19:55:04 -0400

    done cross environment setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(PORT, () => {
+    Logger.log(`Nest service running on port: ${PORT}`);
+  });
 }
 bootstrap();
