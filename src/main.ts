39f0e54701ffb7fddae5cbbdaf603fb6dfa78d42
Murat Jummyyev <muratjummyyev@Murats-MacBook-Air.local>
2022-08-10 21:40:44 -0400

    global validation set and user dto validation added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,6 +6,12 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3000;
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: true,
+    }),
+  );
 
   await app.listen(PORT, () => {
     Logger.log(`Nest service running on port: ${PORT}`);
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,9 @@
+import { Matches } from 'class-validator';
+
 export class UpdateUserDto {
+  @Matches(/[a-zA-Z]{2,}/)
   firstName?: string;
+
+  @Matches(/[a-zA-Z]{2,}/)
   lastName?: string;
 }
